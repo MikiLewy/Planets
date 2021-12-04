4da46d72d13fcc85bb7c3fe69547530683511439
Mikolaj <m.lewandowski.0231@gmail.com>
2021-12-04 16:22:57 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import background from "assets/background-stars.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Planets</h1>
+    </div>
+  );
+}
+export default App;
